5f9b10744b3a2fd9f91f0575cd8d9175d26a78ab
andrew <andrew.cai@primerobotics.com>
2020-01-10 16:32:54 +0800

    Show the Problem Off

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,23 +3,3 @@
     <h1>This is an about page</h1>
   </div>
 </template>
-
-<script>
-import * as PIXI from 'pixi.js';
-import ViewUtils from './builder-pixi/utils/ViewUtils';
-
-export default {
-  async created() {
-    const graphics = new PIXI.Graphics();
-    const width = -2;
-    const height = 4;
-    graphics.clear();
-    graphics.beginFill(0x0);
-    graphics.drawRect(0, 0, width, height);
-    graphics.endFill();
-    window.graphics = graphics;
-    // eslint-disable-next-line no-underscore-dangle
-    console.log('graphics', graphics.width, graphics.height, graphics._bounds);
-  },
-};
-</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import * as PIXI from 'pixi.js';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  async created() {
+    const app = new PIXI.Application();
+    document.body.appendChild(app.view);
+
+    const graphics = new PIXI.Graphics();
+    // const width = 60;
+    const width = -60;
+    const height = 40;
+    graphics.clear();
+    graphics.beginFill(0xff0000);
+    graphics.drawRect(100, 100, width, height);
+    graphics.endFill();
+
+    app.stage.addChild(graphics);
+
+    console.log('width', graphics.width, 'height', graphics.height);
+    // eslint-disable-next-line no-underscore-dangle
+    console.log('bounds', graphics._bounds, graphics.getBounds());
+    window.graphics = graphics;
   },
 };
 </script>
